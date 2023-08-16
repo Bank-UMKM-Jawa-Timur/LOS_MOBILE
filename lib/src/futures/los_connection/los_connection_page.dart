85b45d3624f4fedf2f786aab50826b5fa924d3bb
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2023-08-16 19:55:17 +0700

    menambahkan willPopScope di losConnection

--- lib/src/futures/los_connection/los_connection_page.dart
+++ lib/src/futures/los_connection/los_connection_page.dart
@@ -6,52 +6,59 @@ import 'package:los_mobile/utils/colors.dart';
 
 class LosConnectionPage extends StatelessWidget {
   const LosConnectionPage({super.key});
+  Future<bool> _onPop() async {
+    return false;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: Get.width / 1,
-              height: Get.height / 3.5,
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                image: AssetImage("assets/images/no_connection.png"),
-              )),
-            ),
-            spaceHeightMedium,
-            const Text(
-              "Koneksi Internet Teputus!",
-              style: textBoldDarkVeryLarge,
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-              child: Text(
-                "Uppss, koneksi anda terputus silahkan hubungkan ulang koneksi anda",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 14, color: mGreyFlatColor),
+    return WillPopScope(
+      onWillPop: _onPop,
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: Get.width / 1,
+                height: Get.height / 3.5,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage("assets/images/no_connection.png"),
+                )),
               ),
-            ),
-            SizedBox(
-              width: 120,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: mPrimaryColor),
-                onPressed: () async {
-                  ConnectivityResult result =
-                      await Connectivity().checkConnectivity();
-                  if (result == ConnectivityResult.none) {
-                    debugPrint("NO INTERNET");
-                  } else {
-                    Get.back();
-                  }
-                },
-                child: const Text("Coba lagi"),
+              spaceHeightMedium,
+              const Text(
+                "Koneksi Internet Teputus!",
+                style: textBoldDarkVeryLarge,
               ),
-            )
-          ],
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                child: Text(
+                  "Uppss, koneksi anda terputus silahkan hubungkan ulang koneksi anda",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 14, color: mGreyFlatColor),
+                ),
+              ),
+              SizedBox(
+                width: 120,
+                child: ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: mPrimaryColor),
+                  onPressed: () async {
+                    ConnectivityResult result =
+                        await Connectivity().checkConnectivity();
+                    if (result == ConnectivityResult.none) {
+                      debugPrint("NO INTERNET");
+                    } else {
+                      Get.back();
+                    }
+                  },
+                  child: const Text("Coba lagi"),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
